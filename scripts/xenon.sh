258cf1e1ef9b6a619e1b5adff676a6cbfe303b7d
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-01-24 19:04:09 +0200

    Delete xenon.sh

--- scripts/xenon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "=================== SCRIPT ================"
-cat ./scripts/xenon.sh
-echo "=================== END ==================="
-
-# Cyclomatic Complexity 6-10, Rank B, Risk low - well structured and stable block
-xenon . --max-absolute B --max-modules A --max-average A --e behave-parallel.py,core/api/appointments_api.py

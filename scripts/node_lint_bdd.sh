f5038c05ed4e6f96391eac13c4ab96fa968d6d58
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-01-24 21:36:14 +0200

    Delete node_lint_bdd.sh

--- scripts/node_lint_bdd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "=================== SCRIPT ================"
-cat ./scripts/node_lint_bdd.sh
-echo "=================== END ==================="
-
-# lint Gherkin via NodeJS
-npm run lint-cucumber

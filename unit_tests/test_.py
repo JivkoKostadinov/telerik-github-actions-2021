8037e70f8807dc6a932a5e5aa49aba0dd5299e1f
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-01-24 18:53:05 +0200

    Update test_.py

--- unit_tests/test_.py
+++ unit_tests/test_.py
@@ -1,20 +1,20 @@
 from future import standard_library
-standard_library.install_aliases()
 import unittest  # noqa
 import sys  # noqa
 import os  # noqa
 
 class Test(unittest.TestCase):
 
+    
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    @contextmanager
     def captured_output(self):
         pass
 
+    
 if __name__ == '__main__':
     unittest.main()

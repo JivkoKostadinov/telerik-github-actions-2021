e28a0eebde1233ed22b4c22f2440d511b8cae040
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-01-24 19:02:25 +0200

    Delete run_unit_tests.sh

--- scripts/run_unit_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# python -m coverage xml --include=core
-
-if [[ "$OSTYPE" == "msys"* ]];
-then
-  python -m nose unit_tests/* --with-xunit --with-coverage --cover-erase --cover-package core -s
-else
-  python3 -m nose unit_tests/* --with-xunit --with-coverage --cover-erase --cover-package core -s
-fi

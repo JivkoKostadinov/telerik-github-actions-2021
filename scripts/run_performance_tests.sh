bb058650aef7e5ceb7f38bd2956a7b7b157c26ab
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-02-02 14:35:22 +0200

    Update run_performance_tests.sh

--- scripts/run_performance_tests.sh
+++ scripts/run_performance_tests.sh
@@ -1,1 +1,1 @@
-locust -f tests/performance_tests.py --headless --run-time 1s --users 2 --hatch-rate 10
+locust -f performance_tests/performance_tests.py --headless --run-time 1s --users 2 --hatch-rate 10

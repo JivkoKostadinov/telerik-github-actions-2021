8b9a965dff5990edf5eaac56d2e8ee3a6b707209
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-02-02 14:09:56 +0200

    Update integrations_tests.py

--- integrations_tests/integrations_tests.py
+++ integrations_tests/integrations_tests.py
@@ -1,1 +1,20 @@
 
+import unittest  # noqa
+import sys  # noqa
+import os  # noqa
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def captured_output(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

eeb0a7b928977ed14847cff5decb15be1b4be54f
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-02-02 14:24:18 +0200

    Update performance_tests.py

--- performance_tests/performance_tests.py
+++ performance_tests/performance_tests.py
@@ -1,1 +1,13 @@
+from locust import HttpUser
 
+class DemoUser(HttpUser):
+
+    @task
+    def login(self):
+        pass
+
+    def on_start(self):
+        pass
+      
+    def on_end(self):
+        pass      

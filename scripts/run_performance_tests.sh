da6c4bfe2c558e8c8d6dbc34874e8e26a955adc1
JivkoKostadinov <kostadinov.j.c@gmail.com>
2022-02-02 14:47:59 +0200

    Update run_performance_tests.sh

--- scripts/run_performance_tests.sh
+++ scripts/run_performance_tests.sh
@@ -1,1 +1,1 @@
-locust -f performance_tests/performance_tests.py --headless --run-time 1s --users 2 --hatch-rate 10
+locust -f performance_tests/performance_tests.py --headless --host https://api.exchangeratesapi.io/v1/latest --run-time 1s --users 2 --hatch-rate 10
